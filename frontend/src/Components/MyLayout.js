07b82a3333b6cf46d9e5528bc315848cc379b0de
Milan Magyar <milan.magyar@gmail.com>
2020-01-06 16:53:34 +0100

    Hide Properties in the sidebar

--- frontend/src/Components/MyLayout.js
+++ frontend/src/Components/MyLayout.js
@@ -1,29 +1,60 @@
-import React, { forwardRef } from 'react'
-import { AppBar, Layout, UserMenu, MenuItemLink } from 'react-admin'
+import React, { forwardRef, Fragment } from 'react'
+import { AppBar, Layout, UserMenu, MenuItemLink, Sidebar, usePermissions } from 'react-admin'
+import { makeStyles } from '@material-ui/core/styles'
 import SettingsIcon from '@material-ui/icons/Settings'
 import { getUserData } from '../authProvider'
+
+const useSidebarStyles = (permissions) => {
+  const styles = {}
+
+  if (permissions === 'user') {
+    styles.drawerPaper = {
+      '& a[href="#/Property"]': {
+        display: 'none'
+      }
+    }
+  }
+
+  return makeStyles(styles)()
+}
+
+const MySidebar = props => {
+  const { permissions } = usePermissions()
+  const classes = useSidebarStyles(permissions)
+
+  return <Sidebar classes={classes} {...props} />
+}
 
 const ProfileMenu = forwardRef(({ onClick }, ref) => {
   const userData = getUserData()
 
   return (
-    <MenuItemLink
-      ref={ref}
-      to={`/User/${userData.id}`}
-      primaryText="Profile"
-      leftIcon={<SettingsIcon/>}
-      onClick={onClick}
-    />
+    <Fragment>
+      <MenuItemLink
+        ref={ref}
+        to={`/User/${userData.id}`}
+        primaryText="Profile"
+        leftIcon={<SettingsIcon/>}
+        onClick={onClick}
+      />
+      <MenuItemLink
+        ref={ref}
+        to={`/Property`}
+        primaryText="Properties"
+        leftIcon={<SettingsIcon/>}
+        onClick={onClick}
+      />
+    </Fragment>
   )
-});
+})
 
 const MyUserMenu = props => (
   <UserMenu {...props}>
     <ProfileMenu />
   </UserMenu>
-);
+)
 
 const MyAppBar = props => <AppBar {...props} userMenu={<MyUserMenu />} />
-const MyLayout = props => <Layout {...props} appBar={MyAppBar} />
+const MyLayout = props => <Layout {...props} appBar={MyAppBar} sidebar={MySidebar} />
 
 export default MyLayout
