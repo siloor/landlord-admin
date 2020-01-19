07711cb25e7ff05ba7ac09f2df7ed40af1f2e73d
Milan Magyar <milan.magyar@gmail.com>
2020-01-19 09:31:39 +0100

    Add email translation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
 import { Admin, Resource } from 'react-admin'
+import polyglotI18nProvider from 'ra-i18n-polyglot'
+import englishMessages from 'ra-language-english'
 import { createMuiTheme } from '@material-ui/core/styles'
 import { deepPurple } from '@material-ui/core/colors'
 import People from '@material-ui/icons/People'
+import _ from 'lodash'
 
 import buildDataProvider, { propertyRoutePrefix } from './dataProvider'
 import authProvider from './authProvider'
@@ -22,7 +25,19 @@ const theme = createMuiTheme({
 
 const customRoutes = [
   <Route exact path={`/${propertyRoutePrefix}Dashboard`} component={PropertyDashboard} />
-];
+]
+
+const messages = {
+  en: _.merge(englishMessages, {
+    ra: {
+      auth: {
+        email: 'Email'
+      }
+    }
+  })
+}
+
+const i18nProvider = polyglotI18nProvider(locale => messages[locale])
 
 class App extends Component {
   constructor () {
@@ -45,7 +60,7 @@ class App extends Component {
     }
 
     return (
-      <Admin dataProvider={dataProvider} authProvider={authProvider} theme={theme} dashboard={Dashboard} layout={MyLayout} customRoutes={customRoutes} loginPage={MyLogin}>
+      <Admin dataProvider={dataProvider} authProvider={authProvider} theme={theme} dashboard={Dashboard} layout={MyLayout} customRoutes={customRoutes} loginPage={MyLogin} i18nProvider={i18nProvider}>
         {permissions => [
           <Resource name={'User'} icon={People} list={permissions === 'admin' ? UserList : null} edit={UserEdit} create={permissions === 'admin' ? UserCreate : null} />,
           <Resource name='Property' list={PropertyList} edit={PropertyEdit} create={PropertyCreate} />
